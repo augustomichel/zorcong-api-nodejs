8a1e888458cc8358fe5578bfe9925aef62c436a8
augustomichel <augustomichel@gmail.com>
2022-04-04 18:06:23 -0300

    testes simples com jest

--- test/jest.test.js
+++ test/jest.test.js
@@ -1,1 +1,9 @@
-test('Should', () => {});
+test('Should', () => {
+  let number = null;
+  expect(number).toBeNull();
+});
+
+teste('Trabalahr com objetos', () => {
+  const obj = { name: 'john', mail: 'john@teste.com' };
+  expect(obj).toHaveProperty('name');
+});
